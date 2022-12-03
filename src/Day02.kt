45b68d62249ab6572a5d454dc694ffbcf6e30f5e
annagergaly <anna.gergaly@gmail.com>
2022-12-03 01:36:34 +0100

    Day 2 c:

---
+++ src/Day02.kt
@@ -0,0 +1,39 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.sumOf {
+            when (it) {
+                "A X" -> 4
+                "A Y" -> 8
+                "A Z" -> 3
+                "B X" -> 1
+                "B Y" -> 5
+                "B Z" -> 9
+                "C X" -> 7
+                "C Y" -> 2
+                "C Z" -> 6
+                else -> 0
+            }.toInt()
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.sumOf {
+            when (it) {
+                "A X" -> 3
+                "A Y" -> 4
+                "A Z" -> 8
+                "B X" -> 1
+                "B Y" -> 5
+                "B Z" -> 9
+                "C X" -> 2
+                "C Y" -> 6
+                "C Z" -> 7
+                else -> 0
+            }.toInt()
+        }
+    }
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
